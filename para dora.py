9c33930a375be60d847800562068c3d90865bdd9
Andres Alvarez <andresalvarezabogado@gmail.com>
2022-09-14 18:46:33 -0400

    Mi primer repositorio dedicado a mi esposa Dora Figuera, comence a programar el 06/04/2022 y mi primer programa fue el 12/04/2022 para mi amada justo en nuestro aniversario numero 2

---
+++ para dora.py
@@ -0,0 +1,20 @@
+print ("RESPONDA LAS SIGUIENTES PREGUNTAS")
+
+nombre1=input("Escriba sus dos nombres completos:   ")
+apellido=input("Escriba sus dos apellidos completos:   ")
+nombre2=input("Escriba los dos nombres completos de su amado compañero:"   )
+apellido2=input("Escriba los dos apellidos de su amado compañero:    ")
+
+print ("Ahora bien", nombre1,apellido, "Conteste las siguientes preguntas")
+pregunta1=input("¿Ama usted a su amado antes mencionado?")
+pregunta2=input("¿Desea vivir el resto de su vida con Andres Eduardo Alvarez Torres?  ")
+pregunta3=input("¿Promete no volver a ponerse tan pero tan bravisima con su Amando?   ")
+print ("TOMANDO EN CONSIDERACION TODAS SUS RESPUESTAS")
+pregunta4=input("Andres desea hacerle una pregunta, ¿quiere qque le haga esa pregunta? (responda si o no):"   )
+print ("la pregunta es")
+pregunta5=input("¿QUIERES CASARTE CONMIGO?"   )
+print ("SI LA PRESPUESTA ES SI ENTONCES VOLTEA A VER A TU AMADO")
+print ("ESPERO QUE SEAMOS FELICES POR SIEMPRE")
+print (nombre1,apellido, "y", nombre2,apellido2)
+print ("Barquisimeto hoy 12 de abril de 2022")
+
---
+++ peticion de matrimonio y entrega de anillo de compromiso.py
@@ -0,0 +1,19 @@
+RESPONDA LAS SIGUIENTES PREGUNTAS
+Escriba sus dos nombres completos:   Dora Josefina
+Escriba sus dos apellidos completos:   Figuera Rodriguez
+Escriba los dos nombres completos de su amado compañero:Andres Eduardo
+Escriba los dos apellidos de su amado compañero:    Alvarez Torres
+Ahora bien Dora Josefina Figuera Rodriguez Conteste las siguientes preguntas
+¿Ama usted a su amado antes mencionado?Con todo mi corazon
+¿Desea vivir el resto de su vida con Andres Eduardo Alvarez Torres?  El resto de mi vida. y si hay una eternidad, deseo que sea junto a El
+¿Promete no volver a ponerse tan pero tan bravisima con su Amando?   lo prometo con mi garrita
+TOMANDO EN CONSIDERACION TODAS SUS RESPUESTAS
+Andres desea hacerle una pregunta, ¿quiere qque le haga esa pregunta? (responda si o no):si
+la pregunta es
+¿QUIERES CASARTE CONMIGO?con mi alma
+SI LA PRESPUESTA ES SI ENTONCES VOLTEA A VER A TU AMADO
+ESPERO QUE SEAMOS FELICES POR SIEMPRE
+Dora Josefina Figuera Rodriguez y Andres Eduardo Alvarez Torres
+Barquisimeto hoy 12 de abril de 2022
+
+***Repl Closed***
